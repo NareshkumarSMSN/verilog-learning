6f200b9ff1e73209461d12da9b391a3092878881
NareshkumarSMSN <n6k5sm@gmail.com>
2026-01-26 16:35:03 +0530

    Replacing 4 bit ripple adder with 4 bit carry lookahead adder

--- 16bit_adder/16_bit_adder/4_bit_adder/adder_4bit.v
+++ 16bit_adder/16_bit_adder/4_bit_adder/adder_4bit.v
@@ -4,6 +4,19 @@ input cin;
 output[3:0] Z;
 output carry;
 
-assign {carry,Z} = X+Y+cin;
+wire [3:0] G,P;
+wire c1,c2,c3;
 
+assign G=X&Y;
+assign P=X^Y;
+
+assign c1 = G[0]|(P[0] & cin);
+assign c2 = G[1]|(P[1] & G[0])| (P[1] & P[0] & cin);
+assign c3 = G[2]|(P[2] & G[1])| (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & cin);
+assign carry = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & cin);
+
+assign Z[0] = P[0] ^ cin;
+assign Z[1] = P[1] ^ c1;
+assign Z[2] = P[2] ^ c2;
+assign Z[3] = P[3] ^ c3;
 endmodule
